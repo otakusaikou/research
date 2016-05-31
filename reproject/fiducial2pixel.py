0f88a7d99a85ebe9ef8dbb474b4a3eb210a2b2de
otakusaikou <otakuzyoutou@gmail.com>
2016-05-31 18:22:08 +0800

    Initial commit

---
+++ reproject/fiducial2pixel.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Distort image coordinates and transform it from mm to pixel."""
+import numpy as np
+from sympy import lambdify
+from sympy import Matrix
+from sympy import sqrt
+from sympy import symbols
+import sys
+
+
+def getIO(IOFileName):
+    """Read interior orientation information from file."""
+    fin = open(IOFileName)
+    data = map(lambda x: float(x), fin.readline().split())
+    fin.close()
+
+    # Define interior orientation parameters
+    IO = {
+        'f': data[0],
+        'xp': data[1],
+        'yp': data[2],
+        'Fw': data[3],
+        'Fh': data[4],
+        'px': data[5],
+        'k1': data[6],
+        'k2': data[7],
+        'k3': data[8],
+        'p1': data[9],
+        'p2': data[10]}
+
+    return IO
+
+
+def xy2RowCol(IO, xcArr, ycArr):
+    """Transform given image coordinates from (x, y) to (row, colum)."""
+    # Symbols for unknown parameters
+    xps, yps = symbols("xp yp")
+
+    # Symbols for constants
+    xcs, ycs = symbols("xc yc")
+
+    # Get coordinates of principal point relative to fiducial axis
+    x0 = IO['xp'] - IO['Fw']/2
+    y0 = -(IO['yp'] - IO['Fh']/2)
+
+    # Compute distance from principal point to image point
+    xbar = xps - x0
+    ybar = yps - y0
+    r = sqrt(xbar**2 + ybar**2)
+
+    # List observation equations
+    F = Matrix([
+        xcs + x0 - xbar * (r**2*IO['k1']+r**4*IO['k2']+r**6*IO['k3']) -
+        (IO['p1']*(r**2+2*xbar**2)+2*IO['p2']*xbar*ybar) - xps,
+        ycs + y0 - ybar * (r**2*IO['k1']+r**4*IO['k2']+r**6*IO['k3']) -
+        (2*IO['p1']*xbar*ybar+IO['p2']*(r**2+2*ybar**2)) - yps])
+
+    # Create function object for F and its jacobian matrix
+    JFx = F.jacobian([xps, yps])
+    FuncJFx = lambdify((xcs, ycs, xps, yps), JFx)
+    FuncF = lambdify((xcs, ycs, xps, yps), F)
+
+    # Compute corresponding (row, column) values from given (x, y)
+    rowColArr = np.empty((0, 2))
+    idx = 0
+    curValue = 0    # Current percentage of completion
+    ptNum = len(xcArr)
+    sys.stdout.write("(x, y) -> (row, col): %3d%%" % 0)
+
+    for xc, yc in zip(xcArr, ycArr):
+        X0 = np.matrix([xc, yc]).T      # Initial values for unknown parameters
+        dX = np.ones(1)                 # Initial value for iteration
+
+        # Iteration process
+        while abs(dX.sum()) > 10**-12:
+            # Compute coefficient matrix and constants matrix
+            B = FuncJFx(*tuple(np.append((xc, yc), X0)))
+            f = -FuncF(*tuple(np.append((xc, yc), X0)))
+
+            # Solve the unknown parameters
+            N = B.T * B
+            t = B.T * f
+            dX = N.I * t
+
+            X0 += dX            # Update initial values
+
+        xp, yp = map(float, X0)
+
+        # From fiducial coordinate system to row and column
+        col = (xp + IO['Fw']/2.) / IO['px']
+        row = (IO['Fh']/2. - yp) / IO['px']
+
+        rowColArr = np.vstack((rowColArr, [row, col]))
+        idx += 1
+
+        # Update the percentage of completion
+        if curValue < int(100.0 * idx / ptNum):
+            curValue = int(100.0 * idx / ptNum)
+            sys.stdout.write("\b" * 4)
+            sys.stdout.write("%3d%%" % curValue)
+            sys.stdout.flush()
+    sys.stdout.write("\n")
+
+    return rowColArr
+
+
+def main():
+    """Perform a simple test for this module."""
+    IOFileName = 'input/IO.txt'     # Interior orientation parameter
+
+    # Read interior orientation parameter
+    IO = getIO(IOFileName)
+
+    # Get image point coordinates from file
+    data = np.genfromtxt("chkRMS/xy2rc/xy.txt", skip_header=1, dtype=[
+        ('Name', 'S10'), ('x', 'f8'), ('y', 'f8')])
+    x = data['x']
+    y = data['y']
+
+    # Compute corrected coordinates
+    rowColArr = xy2RowCol(IO, x, y)
+
+    print rowColArr
+
+    return 0
+
+if __name__ == '__main__':
+    main()
---
+++ reproject/reproject.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Extracting the corresponding color from image to object points."""
+from fiducial2pixel import getIO
+from fiducial2pixel import xy2RowCol
+import numpy as np
+from numpy import cos
+from numpy import sin
+from scipy.misc import imread
+
+
+np.set_printoptions(suppress=True)  # Disable scientific notation for numpy
+
+
+def getM(Omega, Phi, Kappa):
+    """Compute rotation matrix M."""
+    M = np.matrix([
+        [
+            cos(Phi)*cos(Kappa),
+            sin(Omega)*sin(Phi)*cos(Kappa) + cos(Omega)*sin(Kappa),
+            -cos(Omega)*sin(Phi)*cos(Kappa) + sin(Omega)*sin(Kappa)],
+        [
+            -cos(Phi)*sin(Kappa),
+            -sin(Omega)*sin(Phi)*sin(Kappa) + cos(Omega)*cos(Kappa),
+            cos(Omega)*sin(Phi)*sin(Kappa) + sin(Omega)*cos(Kappa)],
+        [
+            sin(Phi),
+            -sin(Omega)*cos(Phi),
+            cos(Omega)*cos(Phi)]
+        ])
+
+    return M
+
+
+def getxy(IO, EO, objPts):
+    """List observation equations."""
+    f, xo, yo = IO['f'], 0, 0     # IO
+    XL, YL, ZL = EO[:3]         # EO
+    Omega, Phi, Kappa = map(np.radians, EO[3:6])
+    XA, YA, ZA = objPts['X'], objPts['Y'], objPts['Z']      # Object points
+
+    M = getM(Omega, Phi, Kappa)
+
+    r = M[0, 0] * (XA - XL) + M[0, 1] * (YA - YL) + M[0, 2] * (ZA - ZL)
+    s = M[1, 0] * (XA - XL) + M[1, 1] * (YA - YL) + M[1, 2] * (ZA - ZL)
+    q = M[2, 0] * (XA - XL) + M[2, 1] * (YA - YL) + M[2, 2] * (ZA - ZL)
+
+    x = xo - f * (r / q)
+    y = yo - f * (s / q)
+
+    return x, y
+
+
+def getInterpolation(img, x, y):
+    """Resample from right image, using bilinear interpolation."""
+    # Get coordinates of nearest four points
+    x0 = np.floor(x).astype(int)
+    x1 = x0 + 1
+    y0 = np.floor(y).astype(int)
+    y1 = y0 + 1
+
+    # Ensure the coordinates of four points are in the right image extent
+    x0 = np.clip(x0, 0, img.shape[1] - 1)
+    x1 = np.clip(x1, 0, img.shape[1] - 1)
+    y0 = np.clip(y0, 0, img.shape[0] - 1)
+    y1 = np.clip(y1, 0, img.shape[0] - 1)
+
+    # Get intensity of nearest four points
+    Ia = img[y0, x0]  # Upper left corner
+    Ib = img[y1, x0]  # Lower left corner
+    Ic = img[y0, x1]  # Upper right corner
+    Id = img[y1, x1]  # Lower right corner
+
+    # Compute the weight of four points
+    wa = (x1-x) * (y1-y)
+    wb = (x1-x) * (y-y0)
+    wc = (x-x0) * (y1-y)
+    wd = (x-x0) * (y-y0)
+
+    return wa*Ia + wb*Ib + wc*Ic + wd*Id
+
+
+def extractColor(rowColArr, img):
+    """Extract color from image."""
+    rgbArr = np.empty((0, 3))
+
+    for row in rowColArr:
+        rgbArr = np.vstack((rgbArr, getInterpolation(img, row[1], row[0])))
+
+    return rgbArr.astype(int)
+
+
+def main():
+    # Define file names
+    IOFileName = 'IO.txt'
+    EOFileName = 'EO.txt'
+    ptFileName = '../ptCloud/objPts.txt'
+    imgFileName = '../images/P1_L.jpg'
+    outputPtFileName = 'result.txt'
+
+    IO = getIO(IOFileName)
+
+    # XL, YL, ZL, O, P, K, SigXL, SigYL, SigZL, SigO, SigP, SigK
+    EO = np.genfromtxt(EOFileName)
+
+    # Read object points
+    objPts = np.genfromtxt(ptFileName, dtype=[
+        ('Name', 'S15'), ('X', 'f8'), ('Y', 'f8'), ('Z', 'f8')], skip_header=1)
+
+    # Reproject object points to image plane
+    x, y = getxy(IO, EO, objPts)
+    rowColArr = xy2RowCol(IO, x, y)
+
+    # Extract R, G, B color
+    img = imread(imgFileName)
+    RGB = extractColor(rowColArr, img)
+    ptSet = np.concatenate(
+        (objPts[['X', 'Y', 'Z']].view(np.double).reshape(-1, 3), RGB), axis=1)
+
+    # Write out the result
+    np.savetxt(
+        outputPtFileName,
+        ptSet,
+        fmt="%.6f %.6f %.6f %d %d %d",
+        header="X Y Z R G B",
+        comments='')
+
+
+if __name__ == '__main__':
+    main()
